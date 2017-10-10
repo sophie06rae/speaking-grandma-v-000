5ff41127d57b10740c588672e4a3cfaebcca1359
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-10 14:34:02 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speak)
-
-if speak == "I LOVE YOU GRANDMA!"
-puts "I LOVE YOU TOO PUMPKIN!"
-elsif speak.upcase
+if speak.upcase
 puts "NO, NOT SINCE 1938!"
 else
 puts "HUH?! SPEAK UP, SONNY!"
