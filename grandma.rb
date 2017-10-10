4cf2caa1998909c4e805552e7aca4047354aa3cf
Sophie Rae <sophie.rae.06@gmail.com>
2017-10-10 14:31:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -22,5 +22,3 @@ else
 puts "HUH?! SPEAK UP, SONNY!"
 end
 end
-  
-  
